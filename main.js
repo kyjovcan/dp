c3393e08d671f57c90f20983cf4dda17f386257f
Michal Banas <misobanas62@gmail.com>
2018-11-09 03:12:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var cx, cy;
+
+
+var fs = require("fs");
+
+fs.readFile("./mytext.txt", function(text){
+    var textByLine = text.split("\n")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function mm(e){
+    cx = e.clientX;
+    cy = e.clientY;
+    run();
+}
+
+window.onload = run;
+window.onmousemove = mm;
+
+$(function() {
+
+   $("#code-list li").hover(function() {
+      $( this ).toggleClass( "blur-dropshadow" );
+    });
+});
+
+*/
